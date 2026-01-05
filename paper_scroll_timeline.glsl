0b703b1798667fc699ad4476eee4b47defaf9ed5
Sohail Siadat <sohale@gmail.com>
2026-01-05 22:15:02 +0000

    Refactor ym calculation for clarity
    Removed redundant lines and cleaned up the calculation of ym for better readability.

--- paper_scroll_timeline.glsl
+++ paper_scroll_timeline.glsl
@@ -210,18 +210,13 @@ vec2 np_cloud(float t, int num) {
     }
     float ym;
     ym = cc.y0 + sin(cc.yphase + t * cc.yvf) * cc.yamp;
-
-    ym = cc.y0 + sin(cc.yphase + t*cc.yvf)*cc.yamp;
-
-    ym = cc.y0 + sin(cc.yphase+t*cc.yvf)*cc.yamp;
-
-    ym = cc.y0 + sin(cc.yphase+t*cc.yvf) * cc.yamp;
-
-    ym = cc.y0 + sin(cc.yphase + t*cc.yvf)*cc.yamp;
-    ym = cc.y0 + sin(cc.yphase+t*cc.yvf)*cc.yamp;
-
+    ym = cc.y0 + sin(cc.yphase + t * cc.yvf) * cc.yamp;
+    ym = cc.y0 + sin(cc.yphase + t * cc.yvf) * cc.yamp;
+    ym = cc.y0 + sin(cc.yphase + t * cc.yvf) * cc.yamp;
+    ym = cc.y0 + sin(cc.yphase + t * cc.yvf) * cc.yamp;
+    ym = cc.y0 + sin(cc.yphase + t * cc.yvf) * cc.yamp;
+    ym = cc.y0 + sin(cc.yphase + t * cc.yvf) * cc.yamp;
     if (num==11 || num==12 || num==13 || num==14) {
-      ym = cc.y0 + sin(cc.yphase+t*cc.yvf)*cc.yamp;
     }
 
     np = vec2(cc.x0-fract(cc.px) * cc.vx , ym); // x : -1 1
